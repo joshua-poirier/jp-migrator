7f0b8eb7ba169f1fd22dfd224399af239c36c1f3
joshuaadampoirier <josh.poirier@gmail.com>
2023-08-08 20:44:22 -0400

    feat: enable the passing of params through the Http trigger

--- migrator/function_app.py
+++ migrator/function_app.py
@@ -2,3 +2,11 @@ import azure.functions as func
 import logging
 
 app = func.FunctionApp()
+
+
+@app.function_name(name="HttpTrigger2")
+@app.route(route="hello")
+def test_function2(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get("name", "")
+
+    return func.HttpResponse(f"HttpTrigger2 function processed {name}'s request")
